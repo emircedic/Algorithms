b27143772e018221c6b1224dd2f1ccf72ef89379
Emir Ćedić <45953377+emircedic@users.noreply.github.com>
2025-05-21 22:27:20 +0200

    Added Quick-Select.

--- Algorithms/Sorting Algorithms/QuickSort.cs
+++ Algorithms/Sorting Algorithms/QuickSort.cs
@@ -38,4 +38,41 @@
             QuickSortInternal(nextAvailableSlot + 1, endIndex, nums);
         }
     }
+
+    /// <summary>
+    /// Quick-select is a variant of quick-sort where we sort only the part of the array where the target value is located.
+    /// On every iteration in best case we remove half the elements of the array.
+    /// Time: Omega(n) - Theta(n)- O(n* n)
+    /// Space: Omega(logn) - Theta(logn)- O(n)
+    /// </summary>
+    public class QuickSelect
+    {
+        private int QuickSelectInternal(int[] nums, int left, int right, int k)
+        {
+            var pivotValue = nums[right - 1];
+            var swapIndex = left;
+
+            for (int i = left; i < right; i++)
+            {
+                if (nums[i] < pivotValue)
+                {
+                    var tempValue = nums[i];
+                    nums[i] = nums[swapIndex];
+                    nums[swapIndex] = tempValue;
+
+                    swapIndex++;
+                }
+            }
+
+            nums[right - 1] = nums[swapIndex];
+            nums[swapIndex] = pivotValue;
+
+            if ((nums.Length - k) < swapIndex)
+                return QuickSelectInternal(nums, left, swapIndex, k);
+            else if ((nums.Length - k) > swapIndex)
+                return QuickSelectInternal(nums, swapIndex + 1, right, k);
+            else
+                return nums[swapIndex];
+        }
+    }
 }
