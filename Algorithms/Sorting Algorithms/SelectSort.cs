0cbc4db62e3d0dacfdf0701acc21b9b56def0676
Emir <45953377+emircedic@users.noreply.github.com>
2025-07-02 23:23:40 +0200

    Update SelectSort.cs

--- Algorithms/Sorting Algorithms/SelectSort.cs
+++ Algorithms/Sorting Algorithms/SelectSort.cs
@@ -1,22 +1,24 @@
 ﻿namespace Algorithms
 {
+     // Time complexity: O (n*n)
+     // Space complexity: O (1)
     public class SelectSort
     {
         public int[] SortArray(int[] nums)
         {
             for (int i = 0; i < nums.Length; i++)
             {
-                int currentLowestValueIndex = i;
+                int minValueIndex = i;
 
                 for (int j = i + 1; j < nums.Length; j++)
                 {
-                    if (nums[j] < nums[currentLowestValueIndex])
-                        currentLowestValueIndex = j;
+                    if (nums[j] < nums[minValueIndex])
+                        minValueIndex = j;
                 }
 
                 int tempValue = nums[i];
-                nums[i] = nums[currentLowestValueIndex];
-                nums[currentLowestValueIndex] = tempValue;
+                nums[i] = nums[minValueIndex];
+                nums[minValueIndex] = tempValue;
             }
 
             return nums;
